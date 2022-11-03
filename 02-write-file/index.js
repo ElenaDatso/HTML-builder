faa8cded8edced3fe743eaf0e5ae011f1ceac7d2
Olena Datso <elena.shcheglova@gmail.com>
2022-11-03 17:48:40 +0200

    feature: add task 2 functionality

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+// const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, error } = process;
+
+const textFile = path.join(__dirname, 'text.txt');
+
+fs.writeFile(textFile, '', (err) => {
+  if (err) throw err;
+});
+
+stdout.write('Введите текст\n');
+stdin.on('data', (data) => {
+  fs.appendFile(textFile, data, (err) => {
+    if (err) throw err;
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('Спасибо что заглянули!');
+  process.exit();
+});
