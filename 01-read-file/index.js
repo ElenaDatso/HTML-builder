3dd80790e16abae1aa24b3647214b8aadb4b4335
Olena Datso <elena.shcheglova@gmail.com>
2022-11-03 11:28:13 +0200

    feature: add read file function for task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+const fileToRead = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+fileToRead.on('data', (data) => {
+  stdout.write(data)}
+);
