d16e417d17979382ec5b8f6b9a3e469b560b541f
Olena Datso <elena.shcheglova@gmail.com>
2022-11-03 23:32:25 +0200

    refactor: add kb corvert

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,40 +4,28 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 const fsPomis = require('fs/promises');
 
+fs.readdir(
+  folderPath,
+  { encoding: 'utf-8', withFileTypes: true },
+  (err, file) => {
+    if (err) throw err;
 
-// const fileSize = await stat(path.join(dirName, file.name));
+    for (let i = 0; i < file.length; i++) {
+      if (file[i].isFile()) {
+        const fileName = file[i].name;
+        const fileDir = path.join(folderPath, fileName);
+        const fileOnlyName = path.basename(fileDir).split('.')[0];
+        const fileOnlyExt = path.extname(fileDir).slice(1);
+        const fileSize = fsPomis
+          .stat(fileDir, (er, data) => {
+            if (er) throw er;
+            return data;
+          });
 
-async function filesInFolder() {
-  try {
-	fs.readdir(
-    folderPath,
-    { encoding: 'utf-8', withFileTypes: true },
-    (err, file) => {
-      if (err) throw err;
-
-      for (let i = 0; i < file.length; i++) {
-        if (file[i].isFile()) {
-
-          const fileName = file[i].name;
-          const fileDir = path.join(folderPath, fileName);
-          const fileOnlyName = path.basename(fileDir).split('.')[0];
-          const fileOnlyExt = path.extname(fileDir).slice(1);
-		  const fileSize = fsPomis.stat(fileDir, (er, data) => {
-			if (er) throw er;
-			return data
-		  }).then(stats => stats);
-
-		  fileSize.then(function(result){
-			console.log(fileOnlyName, '-', fileOnlyExt, '-' , result.size);
-		  })
-
-        }
+        fileSize.then(function (result) {
+          console.log(`${fileOnlyName} - ${fileOnlyExt} - ${result.size}kb`);
+        });
       }
     }
-  );
-	
-}catch (err) {
-    console.error(err);
   }
-};
-filesInFolder()
+);
