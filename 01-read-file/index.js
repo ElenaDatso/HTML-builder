3c384c1adcb08931a4a8f500a0a01f45f344b3b9
Olena Datso <elena.shcheglova@gmail.com>
2022-11-03 14:04:19 +0200

    refactor: replace an output method

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const {stdout} = process;
+// const {stdout} = process;
 const fileToRead = fs.createReadStream(path.join(__dirname, 'text.txt'));
 
 fileToRead.on('data', (data) => {
-  stdout.write(data)}
+  console.log(data.toString())}
 );
