16e7796b9bc96b92e55be40fec638283801f45a1
Olena Datso <elena.shcheglova@gmail.com>
2022-11-05 16:53:04 +0200

    refactor: if exit check listener

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,10 @@
 // const process = require('process');
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout, error } = process;
+const { stdin: input, stdout: output } = process;
+const readLine = require('readline');
+
+const rl = readLine.createInterface({input, output});
 
 const textFile = path.join(__dirname, 'text.txt');
 
@@ -9,22 +12,25 @@ fs.writeFile(textFile, '', (err) => {
   if (err) throw err;
 });
 
-stdout.write('Введите текст\n');
+output.write('Введите текст\n');
 
-stdin.on('data', (data) => {
-  if (data.toString().includes('exit')) {
-    console.log('Спасибо что заглянули!');
-    process.exit();
-  }
+input.on('data', (data) => {
   fs.appendFile(textFile, data, (err) => {
     if (err) throw err;
   });
 });
 
-
-
-process.on('SIGINT', () => {
+rl.on('SIGINT', () => {
   console.log('Спасибо что заглянули!');
   process.exit();
 });
+
+rl.on('line', (input) => {
+	if (input === 'exit'){
+    console.log('Спасибо что заглянули!');
+    process.exit();
+	}
+
+})
+
 
