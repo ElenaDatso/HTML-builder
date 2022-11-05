ea4c17f088f63e5f7f2d444bc1989b06a9a12a7e
Olena Datso <elena.shcheglova@gmail.com>
2022-11-05 17:15:07 +0200

    refactor: correct bites to kb

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -23,7 +23,7 @@ fs.readdir(
           });
 
         fileSize.then(function (result) {
-          console.log(`${fileOnlyName} - ${fileOnlyExt} - ${result.size/1000} kb`);
+          console.log(`${fileOnlyName} - ${fileOnlyExt} - ${Math.round(result.size*0.0009765625)} kb`);
         });
       }
     }
